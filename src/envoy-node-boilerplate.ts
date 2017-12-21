c60a49f4f7515dda3f65a2373c73c48f07cf2a38
Yingyu Cheng <yingyu@tubi.tv>
2017-12-22 07:53:13 +0800

    build and rollup

--- src/envoy-node-boilerplate.ts
+++ src/envoy-node-boilerplate.ts
@@ -1,11 +1,13 @@
 export * from "./envoy-context";
 export * from "./envoy-fetch";
-export * from "./envoy-request-params";
-export * from "./envoy-http-request-params";
 export * from "./envoy-grpc-request-params";
+export * from "./envoy-http-client";
+export * from "./envoy-http-request-params";
+export * from "./envoy-request-params";
 
 export { default as EnvoyContext } from "./envoy-context";
 export { default as envoyFetch } from "./envoy-fetch";
-export { default as EnvoyHttpRequestParams } from "./envoy-http-request-params";
 export { default as EnvoyGrpcRequestParams } from "./envoy-grpc-request-params";
+export { default as EnvoyHttpClient } from "./envoy-http-client";
+export { default as EnvoyHttpRequestParams } from "./envoy-http-request-params";
 export { default as envoyProtoDecorator } from "./envoy-proto-decorator";
